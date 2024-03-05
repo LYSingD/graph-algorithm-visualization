14001d9c0899865c50cc27dfed78330b9636035b
LYSingD <me@davidlys.dev>
2024-03-04 22:11:00 -0500

    Initial astar search algorithm

---
+++ astar-search/src/app/algorithms/astar.ts
@@ -0,0 +1,7 @@
+import { TupleType } from 'typescript';
+
+// More A* Search algorithm details could be found in: https://en.wikipedia.org/wiki/A*_search_algorithm
+export default function AStarSearch(maze_grid, starter, target) {
+  let path: Array<TupleType> = [];
+  return path;
+}
